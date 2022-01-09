b0b7b9edf5a342c2964ff51ce5579e46e10ad57f
Ulugbek Yusupxodjayev <ulugbek.yu@gmail.com>
2022-01-09 14:33:19 +0500

    Song controller request logging off

--- app/Http/Controllers/Admin/SongController.php
+++ app/Http/Controllers/Admin/SongController.php
@@ -57,7 +57,6 @@ class SongController extends Controller
 
     public function update(Request $request, Song $song)
     {
-        Log::info($request->all());
         $data = $request->validate([
             'name' => 'required',
             'artist' => 'required',
